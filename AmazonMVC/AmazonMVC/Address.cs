68259843a8e791d1d16a4a707d19338848760882
rahib <rsayed@spartaglobal.com>
2019-03-19 23:40:59 +0000

    Database tables are not being created

---
+++ AmazonMVC/AmazonMVC/Address.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace AmazonMVC
+{
+    public class Address
+    {
+        public int addressID
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public string doorName
+        {
+            get => default(string);
+            set
+            {
+            }
+        }
+
+        public string roadName
+        {
+            get => default(string);
+            set
+            {
+            }
+        }
+
+        public string city
+        {
+            get => default(string);
+            set
+            {
+            }
+        }
+
+        public string postCode
+        {
+            get => default(string);
+            set
+            {
+            }
+        }
+    }
+}
---
+++ AmazonMVC/AmazonMVC/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace AmazonMVC
+{
+    public class Customer
+    {
+        public int customerID
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public string firstName
+        {
+            get => default(string);
+            set
+            {
+            }
+        }
+
+        public string lastName
+        {
+            get => default(string);
+            set
+            {
+            }
+        }
+
+        public Address address
+        {
+            get => default(Address);
+            set
+            {
+            }
+        }
+    }
+}
